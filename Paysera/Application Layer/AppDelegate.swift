89b0c0ffe8d316c6fdb363b3fda13e18ad0aee62
Jeric Miana <JVMiana@abs-cbn.com>
2020-08-08 21:02:50 +0800

    Implement `IQKeyboardManagerSwift`

--- Paysera/Application Layer/AppDelegate.swift
+++ Paysera/Application Layer/AppDelegate.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 import CoreData
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+      
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
