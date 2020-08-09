7235d1a821484f603e479ba47abafbaae11dbce0
Jeric Miana <JVMiana@abs-cbn.com>
2020-08-09 12:25:17 +0800

    Create methods for currency conversion

--- Paysera/Presentation Layer/Scenes/Currency Converter/View/CurrencyConverterViewController.swift
+++ Paysera/Presentation Layer/Scenes/Currency Converter/View/CurrencyConverterViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class CurrencyConverterViewController: UIViewController {
 
@@ -21,12 +23,17 @@ class CurrencyConverterViewController: UIViewController {
     @IBOutlet weak var buyCurrencyButton: UIButton!
     @IBOutlet weak var buyCurrencyLabel: UILabel!
     
+    private let viewModel = CurrencyConverterViewModel()
+    private let disposeBag = DisposeBag()
+
     // MARK: - Lifecycle Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+        configureTextFieldEditing()
+        configureButtonTap()
     }
     
     // MARK: - Configure UI
@@ -37,7 +44,46 @@ class CurrencyConverterViewController: UIViewController {
         submitButton.roundCorners()
     }
     
+    // MARK: - Action Events
     
-
+    private func configureTextFieldEditing() {
+        sellCurrencyTextField.rx.controlEvent([.editingChanged])
+            .debounce(.milliseconds(250), scheduler: MainScheduler.instance)
+            .bind { [weak self] _ in
+                /// Call convert currency API
+                self?.convertCurrency()
+            }.disposed(by: disposeBag)
+    }
+    
+    private func configureButtonTap() {
+        submitButton.rx.tap
+            .debounce(.milliseconds(250), scheduler: MainScheduler.instance)
+            .bind { [weak self] _ in
+                /// TODO: Update/save user balance 
+            }.disposed(by: disposeBag)
+    }
+    
+    // MARK: - Fetch Data
+    
+    private func convertCurrency() {
+        if let sellCurrency = sellCurrencyButton.currentTitle,
+            let buyCurrency = buyCurrencyButton.currentTitle,
+            let amount = sellCurrencyTextField.text {
+            
+            viewModel.convertCurrency(sellCurrency: sellCurrency,
+                                      buyCurrency: buyCurrency,
+                                      amount: amount,
+            success: { [weak self] (data) in
+                DispatchQueue.main.async {
+                    /// Display received amount
+                    self?.buyCurrencyLabel.text = "+" + data.amount
+                }
+            },
+            failure: { (error) in
+                /// Show error message alert
+                print(error)
+            })
+        }
+    }
 }
 
